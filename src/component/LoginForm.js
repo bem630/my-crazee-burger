0f3f233969cd97f5e7c83fe126d9e1b50283f60b
bem630 <bah70495@gmail.com>
2024-04-25 12:45:01 +0100

    ✨ F01: LoginPage > LoginForm > Add form submission

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function LoginForm() {
+    const [inputValue, setInputValue] = useState("")
+    const handleChange = (event) => { 
+        setInputValue(event.target.value)
+     }
+     const handleSubmit = (event) => { 
+        alert(`Bonjour ${inputValue}`)
+        setInputValue("")
+      }
   return (
     <div>
         <h1>Bienvenue chez nous!</h1><br />
         <h2>Connectez-vous</h2>
-        <form action="submit">
-            <input type="text" placeholder='Entrez votre prénom...' required/>
+        <form action="submit" onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                value={inputValue} 
+                placeholder='Entrez votre prénom...' 
+                required
+                onChange={handleChange}
+
+            />
             <button>Accéder à votre espace</button>
         </form>
     </div>
